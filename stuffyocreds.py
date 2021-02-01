8d76308368dc7c78e2100c8571940c8adc2f3727
Ben M <ben.mcdowall@gmail.com>
2021-02-01 10:02:53 +0900

    Update stuffyocreds.py

--- stuffyocreds.py
+++ stuffyocreds.py
@@ -21,7 +21,7 @@ def main(filepath):
 	mycursor2 = mydb.cursor()
 	
 	if not os.path.isfile(filepath):
-		logging.error("File path {} does not exist. Exiting...".format(filepath))
+		logging.error(filepath+" path {} does not exist. Exiting...")
 		sys.exit()
 
 	bag_of_words = {}
@@ -32,13 +32,14 @@ def main(filepath):
 		find = False 
 		for line in fp:
 			try:
+				#Attempt to rip out basic credential formats
 				cd = line.strip().split(':')
-				
 				if len(cd) <2:
 					cd = line.strip().split(';')
 				
 				val = (cd[0], cd[1], filepath)
-					
+				
+				#Check if this file may have been indexed before
 				if cnt==0:
 					checksql = "SELECT * FROM creds2 WHERE username=%s AND password=%s AND filename=%s LIMIT 1";
 					mycursor2.execute(checksql, val)
@@ -50,6 +51,7 @@ def main(filepath):
 					else:
 						logging.debug(filepath+" appears new, continuing")
 						
+				#If we got here it means there is valid credentials, lets start building a query we will later COMMIT
 				sql = "INSERT INTO creds2 (username, password, filename) VALUES (%s, %s, %s)"
 				mycursor.execute(sql, val)
 				cnt += 1
@@ -58,7 +60,7 @@ def main(filepath):
 					logging.debug(filepath+" mid processing commit attempt at "+str(cnt))
 						
 			except mysql.connector.Error as err:
-				logging.error(str(err))
+				logging.error(filepath+" "+str(err))
 				continue
 			except IndexError as err:
 				continue
@@ -78,19 +80,21 @@ if __name__ == '__main__':
 	#Set up the logging
 	logfile = "logger.log"
 	logging.basicConfig(level=logging.DEBUG, filename=logfile, format='%(asctime)s %(levelname)s %(name)s %(message)s')
-	
+	maxthreads = 20
 	mypath = sys.argv[1]  
+	
+	#Start walking the directory to gather all the files
 	myfiles = []
 	for root, dirs, files in os.walk(mypath):
 		for file in files:
 			myfiles.append(os.path.join(root, file))
 	print(len(myfiles))
 
-
+	#Start processing the files
 	for x in myfiles:
-		if threading.activeCount() < 2:
+		if threading.activeCount() < maxthreads:
 			t1 = threading.Thread(target=main,args=(x,))
-			logging.debug("Spinning new thread to sniff file: "+x)
+			logging.debug(x+" spinning new thread to sniff file")
 			t1.start()
-		while threading.activeCount() > 1:
+		while threading.activeCount() > maxthreads-1:
 			btc=1
